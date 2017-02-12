4db54541ca434f5ab4539433aadbd9d200bbde89
rockinroyle <rockinroyle@gmail.com>
2017-02-12 18:48:47 -0500

    cpufreq_govenor.h: cleanup weird conflict derp

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -231,16 +231,6 @@ struct zz_dbs_tuners {
 	unsigned int afs_threshold4;
 };
 
-struct ex_dbs_tuners {
-	unsigned int ignore_nice_load;
-	unsigned int sampling_rate;
-	unsigned int up_threshold;
-	unsigned int down_differential;
-	unsigned int active_floor_freq;
-	unsigned int sampling_down_factor;
-	unsigned int powersave;
-};
-
 /* Common Governor data across policies */
 struct dbs_data;
 struct common_dbs_data {
